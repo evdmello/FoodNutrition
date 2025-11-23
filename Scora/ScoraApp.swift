903ed6881e95a60290f33816281ce03d30fc1739
Errol DMello <evdmello@gmail.com>
2025-11-23 12:20:12 +0530

    Initial Commit

---
+++ Scora/ScoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoraApp.swift
+//  Scora
+//
+//  Created by Errol DMello on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
